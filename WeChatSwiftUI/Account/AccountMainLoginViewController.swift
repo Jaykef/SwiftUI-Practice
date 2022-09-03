736fbb03aaf2af23cee73c6eb8a8daf585412da0
Jaward Sesay <jkef96@icloud.com>
2022-09-03 22:11:55 +0800

    account

---
+++ WeChatSwiftUI/Account/AccountMainLoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AccountMainLoginViewController.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/08/02.
+//
+
+import AsyncDisplayKit
+
+class AccountMainLoginViewController: ASDKViewController<ASDisplayNode> {
+    
+    override init() {
+        super.init(node: ASDisplayNode())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
