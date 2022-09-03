7d31f716df0f63389b65e678c60c88446a279822
Jaward Sesay <jkef96@icloud.com>
2022-09-03 22:10:15 +0800

    app delegates

---
+++ WeChatSwiftUI/AppDelegate/AppDelegate.swift
@@ -0,0 +1,80 @@
+//
+//  AppDelegate.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/08/02.
+//
+
+import UIKit
+import AsyncDisplayKit
+import FLEX
+import WXNavigationBar
+
+typealias RelayCommand = () -> Void
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    var rootViewController: RootViewController!
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        WXNavigationBar.setup()
+        rootViewController = RootViewController()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        if #available(iOS 13, *) {
+            
+        } else {
+          injectLongPressGestureToStatusBar()
+        }
+        AppContext.current.doHeavySetup()
+        
+        UIView.fixTabBarButtonFrame()
+        return true
+    }
+    
+    private func injectLongPressGestureToStatusBar() {
+        if let statusBarWindow = UIApplication.shared.value(forKey: "statusBarWindow") as? UIWindow {
+            let statusBar = statusBarWindow.subviews.first
+            let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressStatusBar(_:)))
+            statusBar?.addGestureRecognizer(longPress)
+        }
+    }
+    
+    @objc private func handleLongPressStatusBar(_ gesture: UILongPressGestureRecognizer) {
+        if AppConfiguration.current() == .debug && gesture.state == .began {
+            FLEXManager.shared.showExplorer()
+        }
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ WeChatSwiftUI/AppDelegate/RootViewController.swift
@@ -0,0 +1,93 @@
+//
+//  RootViewController.swift
+//  WeChatSwift
+//
+//  Created by xu.shuifeng on 2019/12/26.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class RootViewController: ASTabBarController {
+
+    private var chatsVC: SessionViewController!
+    
+    private var contactsVC: ContactsViewController!
+    
+    private var discoverVC: DiscoverViewController!
+    
+    private var meVC: MeViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ASDisableLogging()
+        
+        // Do any additional setup after loading the view.
+        chatsVC = SessionViewController()
+        chatsVC.tabBarItem.selectedImage = HomeTab.chats.selectedImage
+        chatsVC.tabBarItem.image = HomeTab.chats.image
+        chatsVC.tabBarItem.title = LocalizedString("TabBar_MainFrameTitle")
+        chatsVC.tabBarItem.tag = 0
+
+        contactsVC = ContactsViewController()
+        contactsVC.tabBarItem.selectedImage = HomeTab.contacts.selectedImage
+        contactsVC.tabBarItem.image = HomeTab.contacts.image
+        contactsVC.tabBarItem.title = LocalizedString("TabBar_ContactsTitle")
+        contactsVC.tabBarItem.tag = 1
+
+        discoverVC = DiscoverViewController()
+        discoverVC.tabBarItem.selectedImage = HomeTab.discover.selectedImage
+        discoverVC.tabBarItem.image = HomeTab.discover.image
+        discoverVC.tabBarItem.title = LocalizedString("TabBar_FindFriendTitle")
+        discoverVC.tabBarItem.tag = 2
+
+        meVC = MeViewController()
+        meVC.tabBarItem.selectedImage = HomeTab.me.selectedImage
+        meVC.tabBarItem.image = HomeTab.me.image
+        meVC.tabBarItem.title = LocalizedString("TabBar_MoreTitle")
+        meVC.tabBarItem.tag = 3
+        
+        tabBar.unselectedItemTintColor = UIColor(hexString: "#181818")
+        let controllers = [chatsVC, contactsVC, discoverVC, meVC]
+        controllers.forEach { $0?.tabBarItem.setTitleTextAttributes([
+            .font: UIFont.systemFont(ofSize: 9.5)
+        ], for: .normal) }
+        
+        let chatNav = UINavigationController(rootViewController: chatsVC)
+        let contactsNav = UINavigationController(rootViewController: contactsVC)
+        let discoverNav = UINavigationController(rootViewController: discoverVC)
+        let meNav = UINavigationController(rootViewController: meVC)
+        
+        viewControllers = [chatNav, contactsNav, discoverNav, meNav]
+        tabBar.tintColor = Colors.tintColor
+        UIBarButtonItem.appearance().setBackButtonBackgroundImage(UIImage.imageFromColor(.clear), for: .normal, barMetrics: .default)
+    }
+    
+    func handleLanguageDidChanged() {
+        selectedIndex = 3
+        let settingsVC = SettingsViewController()
+        navigationController?.pushViewController(settingsVC, animated: true)
+    }
+}
+
+enum HomeTab: String {
+    case chats
+    case contacts
+    case discover
+    case me
+    
+    var selectedImage: UIImage? {
+        get {
+            let name = "icons_filled_\(rawValue)"
+            return UIImage.SVGImage(named: name, fillColor: Colors.tintColor)
+        }
+    }
+    
+    var image: UIImage? {
+        get {
+            let name = "icons_outlined_\(rawValue)"
+            return UIImage.SVGImage(named: name, fillColor: UIColor.black)
+        }
+    }
+}
