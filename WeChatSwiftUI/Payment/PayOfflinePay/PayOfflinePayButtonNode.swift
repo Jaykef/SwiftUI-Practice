e62394be6969d6e4b751f416a101e06f7bcff1cf
Jaward Sesay <jkef96@icloud.com>
2022-09-04 17:55:10 +0800

    Add files via upload

---
+++ WeChatSwiftUI/Payment/PayOfflinePay/PayOfflinePayButtonNode.swift
@@ -0,0 +1,49 @@
+//
+//  PayOfflinePayButtonNode.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/07/20.
+//  Copyright © 2022 Jaward/Jaykef (苏杰). All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class PayOfflinePayButtonNode: ASButtonNode {
+    
+    private let iconNode = ASImageNode()
+    
+    private let textNode = ASTextNode()
+    
+    private let arrowNode = ASImageNode()
+    
+    init(action: PayOfflinePayAction) {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+        iconNode.image = action.image
+        textNode.attributedText = action.attributedStringForTitle()
+        arrowNode.image = UIImage.as_imageNamed("OfflinePay_Arrorw_8x12_")
+    }
+    
+    override func didLoad() {
+        super.didLoad()
+        
+        backgroundColor = UIColor(hexString: "#FFFFFF", alpha: 0.07)
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        iconNode.style.spacingBefore = 20
+        textNode.style.flexGrow = 1.0
+        arrowNode.style.preferredSize = CGSize(width: 8, height: 12)
+        arrowNode.style.spacingAfter = 20
+        
+        let stack = ASStackLayoutSpec.horizontal()
+        stack.alignItems = .center
+        stack.children = [iconNode, textNode, arrowNode]
+        
+        let insets = UIEdgeInsets(top: 22, left: 0, bottom: 22, right: 0)
+        return ASInsetLayoutSpec(insets: insets, child: stack)
+    }
+    
+}
---
+++ WeChatSwiftUI/Payment/PayOfflinePay/PayOfflinePayViewController.swift
@@ -0,0 +1,87 @@
+//
+//  PayOfflinePayViewController.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/07/20.
+//  Copyright © 2022 Jaward/Jaykef (苏杰). All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class PayOfflinePayViewController: ASDKViewController<ASDisplayNode> {
+    
+    private let scrollNode = ASScrollNode()
+    
+    override init() {
+        super.init(node: ASDisplayNode())
+        node.addSubnode(scrollNode)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        node.backgroundColor = UIColor(hexString: "#439057")
+        navigationItem.title = "收付款"
+        scrollNode.frame = view.bounds
+        
+        setupCardView()
+        setupBottomButtons()
+    }
+    
+    private func setupCardView() {
+        let cardNode = PayOfflinePayQRCodeNode()
+        cardNode.frame = CGRect(x: 10, y: 15, width: view.bounds.width - 20, height: 480)
+        cardNode.cornerRadius = 4
+        cardNode.cornerRoundingType = .defaultSlowCALayer
+        cardNode.backgroundColor = .white
+        scrollNode.addSubnode(cardNode)
+    }
+    
+    private func setupBottomButtons() {
+        let actions = PayOfflinePayAction.allCases
+        let buttonHeight: CGFloat = 66.0
+        let buttonWidth: CGFloat = Constants.screenWidth - 20
+        let containerNode = ASDisplayNode()
+        containerNode.cornerRadius = 2
+        containerNode.cornerRoundingType = .precomposited
+        for (index, action) in actions.enumerated() {
+            let buttonNode = PayOfflinePayButtonNode(action: action)
+            buttonNode.frame = CGRect(x: 0,
+                                      y: CGFloat(index) * (buttonHeight + Constants.lineHeight),
+                                      width: buttonWidth,
+                                      height: buttonHeight)
+            containerNode.addSubnode(buttonNode)
+            if index != actions.count - 1 {
+                let lineNode = ASDisplayNode()
+                lineNode.backgroundColor = UIColor(white: 1, alpha: 0.15)
+                lineNode.frame = CGRect(x: 0, y: CGFloat(index) * buttonHeight, width: buttonWidth, height: Constants.lineHeight)
+                containerNode.addSubnode(lineNode)
+            }
+        }
+        let height = CGFloat(actions.count) * (buttonHeight + Constants.lineHeight) - Constants.lineHeight
+        containerNode.frame = CGRect(x: 10, y: 505, width: Constants.screenWidth - 20, height: height)
+        scrollNode.addSubnode(containerNode)
+        
+        scrollNode.view.contentSize = CGSize(width: Constants.screenWidth, height: 505 + height + Constants.bottomInset)
+    }
+    
+    override var wx_navigationBarBackgroundColor: UIColor? {
+        return UIColor(hexString: "#439057")
+    }
+    
+    override var wx_barTintColor: UIColor? {
+        return UIColor.white
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override var wx_titleTextAttributes: [NSAttributedString.Key : Any]? {
+        return [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+}
