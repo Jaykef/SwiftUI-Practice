af4ddabdaa4301ca52433ebac30e11868ec80d68
Jaward Sesay <jkef96@icloud.com>
2022-09-04 17:31:56 +0800

    Emoticons

---
+++ WeChatSwiftUI/Emoticons/PurchasedEmoticonViewController.swift
@@ -0,0 +1,73 @@
+//
+//  PurchasedEmoticonViewController.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/06/20.
+//  Copyright © 2022 Jaykef. All rights reserved.
+//
+
+import AsyncDisplayKit
+import WXActionSheet
+
+class PurchasedEmoticonViewController: ASDKViewController<ASDisplayNode> {
+    
+    private let tableNode = ASTableNode()
+    
+    override init() {
+        super.init(node: ASDisplayNode())
+        node.addSubnode(tableNode)
+        tableNode.delegate = self
+        tableNode.dataSource = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        node.backgroundColor = Colors.DEFAULT_BACKGROUND_COLOR
+        tableNode.frame = node.bounds
+        tableNode.backgroundColor = .clear
+        tableNode.view.separatorStyle = .none
+        
+        navigationItem.title = LocalizedString("PurchasedEmoticonRecordTitle")
+        
+        let rightButton = UIBarButtonItem(image: Constants.moreImage, style: .plain, target: self, action: #selector(handleRightButtonClicked))
+        navigationItem.rightBarButtonItem = rightButton
+    }
+    
+}
+
+// MARK: - Event Handlers
+extension PurchasedEmoticonViewController {
+    
+    @objc private func handleRightButtonClicked() {
+        let actionSheet = WXActionSheet(cancelButtonTitle: LanguageManager.Common.cancel())
+        actionSheet.add(WXActionSheetItem(title: "恢复购买记录", handler: { _ in
+            
+        }))
+        actionSheet.show()
+    }
+    
+}
+
+// MARK: - ASTableDelegate & ASTableDataSource
+extension PurchasedEmoticonViewController: ASTableDelegate, ASTableDataSource {
+    
+    func numberOfSections(in tableNode: ASTableNode) -> Int {
+        return 0
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        let block: ASCellNodeBlock = {
+            return ASCellNode()
+        }
+        return block
+    }
+}
