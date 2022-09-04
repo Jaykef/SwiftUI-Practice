a102dcc8e01ddb81c16cca95e19a803412fb331c
Jaward Sesay <jkef96@icloud.com>
2022-09-04 13:20:13 +0800

    nearby

---
+++ WeChatSwiftUI/Discover/NearbyPeople/NearbyPeopleCellNode.swift
@@ -0,0 +1,132 @@
+//
+//  NearbyPeopleCellNode.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/08/02.
+//  Copyright © 2022 Jaykef. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class NearbyPeopleCellNode: ASCellNode {
+    
+    private let avatarNode = ASNetworkImageNode()
+    
+    private let nicknameNode = ASTextNode()
+    
+    private let genderNode = ASImageNode()
+    
+    private let distanceNode = ASTextNode()
+    
+    private let albumIconNode = ASImageNode()
+    
+    private let signatureNode = ASTextNode()
+    
+    private let lineNode = ASDisplayNode()
+    
+    init(people: NearbyPeople) {
+        super.init()
+        automaticallyManagesSubnodes = true
+        avatarNode.cornerRadius = 4
+        avatarNode.cornerRoundingType = .precomposited
+        avatarNode.url = people.avatar
+        
+        genderNode.image = people.genderImage
+        
+        nicknameNode.attributedText = people.attributedStringForNickname()
+        distanceNode.attributedText = people.attributedStringForDistance()
+        albumIconNode.image = UIImage(named: "AlbumFlagMark_15x15_")
+        lineNode.backgroundColor = Colors.DEFAULT_SEPARTOR_LINE_COLOR
+    }
+    
+    override func didLoad() {
+        super.didLoad()
+        backgroundColor = Colors.white
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        avatarNode.style.preferredSize = CGSize(width: 46, height: 46)
+        avatarNode.style.spacingBefore = 7
+        
+        genderNode.style.preferredSize = CGSize(width: 18.0, height: 18.0)
+        
+        let nameStack = ASStackLayoutSpec.horizontal()
+        nameStack.spacing = 3
+        nameStack.children = [nicknameNode, genderNode]
+        
+        let descStack = ASStackLayoutSpec.horizontal()
+        descStack.spacing = 3
+        descStack.children = [distanceNode, albumIconNode]
+        
+        let centerVerticalStack = ASStackLayoutSpec.vertical()
+        centerVerticalStack.spacing = 6
+        centerVerticalStack.style.spacingBefore = 12
+        centerVerticalStack.children = [nameStack, descStack]
+        
+        signatureNode.style.maxWidth = ASDimension(unit: .points, value: 86)
+        signatureNode.style.spacingAfter = 15
+        
+        let stack = ASStackLayoutSpec.horizontal()
+        stack.style.preferredSize = CGSize(width: constrainedSize.max.width, height: 64)
+        stack.alignItems = .center
+        stack.children = [avatarNode, centerVerticalStack, signatureNode]
+        
+        lineNode.style.preferredSize = CGSize(width: constrainedSize.max.width - 7, height: Constants.lineHeight)
+        lineNode.style.layoutPosition = CGPoint(x: 7, y: 64 - Constants.lineHeight)
+        
+        return ASAbsoluteLayoutSpec(children: [stack, lineNode])
+    }
+    
+}
+
+class NearbyPeople {
+    
+    let userId: String
+    
+    let nickname: String
+    
+    let avatar: URL?
+    
+    var country: String?
+    
+    var province: String?
+    
+    var distance: String? = nil
+    
+    var gender: MockData.Gender
+    
+    init(userId: String, nickname: String, avatar: URL?, gender: MockData.Gender) {
+        self.userId = userId
+        self.nickname = nickname
+        self.avatar = avatar
+        self.gender = gender
+        self.distance = "100米内"
+    }
+    
+    func attributedStringForNickname() -> NSAttributedString {
+        let attributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor: Colors.black
+        ]
+        return NSAttributedString(string: nickname, attributes: attributes)
+    }
+    
+    func attributedStringForDistance() -> NSAttributedString? {
+        guard let distance = distance else { return nil }
+        let attributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12),
+            NSAttributedString.Key.foregroundColor: Colors.DEFAULT_TEXT_GRAY_COLOR
+        ]
+        return NSAttributedString(string: distance, attributes: attributes)
+    }
+    
+    var genderImage: UIImage? {
+        let name = gender == .male ? "Contact_Male_18x18_" : "Contact_Female_18x18_"
+        return UIImage(named: name)
+    }
+    
+    func toContact() -> Contact {
+        return MockFactory.shared.user(with: userId)!.toContact()
+    }
+}
---
+++ WeChatSwiftUI/Discover/NearbyPeople/NearbyPeopleViewController.swift
@@ -0,0 +1,121 @@
+//
+//  NearbyPeopleViewController.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/08/02.
+//  Copyright © 2022 Jaykef. All rights reserved.
+//
+
+import AsyncDisplayKit
+import WXActionSheet
+import CoreLocation
+
+class NearbyPeopleViewController: ASDKViewController<ASDisplayNode> {
+    
+    private let tableNode = ASTableNode(style: .plain)
+    
+    private var dataSource: [NearbyPeople] = []
+    
+    private var errorPlaceholder: NearbyPeopleErrorPlaceholder?
+    
+    override init() {
+        super.init(node: ASDisplayNode())
+        node.addSubnode(tableNode)
+        tableNode.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableNode.delegate = self
+        tableNode.dataSource = self
+        setupDataSource()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        node.backgroundColor = Colors.DEFAULT_BACKGROUND_COLOR
+        tableNode.frame = view.bounds
+        tableNode.backgroundColor = .clear
+        tableNode.view.separatorStyle = .none
+        
+        navigationItem.title = "附近的人"
+        let moreButtonItem = UIBarButtonItem(image: Constants.moreImage, style: .done, target: self, action: #selector(moreButtonClicked))
+        navigationItem.rightBarButtonItem = moreButtonItem
+    }
+    
+    private func checkLocation() {
+        let status = CLLocationManager.authorizationStatus()
+        if status == .denied {
+            
+        }
+    }
+    
+    private func setupDataSource() {
+        dataSource = MockFactory.shared.nearbys()
+    }
+}
+
+// MARK: - Event Handlers
+
+extension NearbyPeopleViewController {
+    
+    @objc private func moreButtonClicked() {
+        let actionSheet = WXActionSheet(cancelButtonTitle: LanguageManager.Common.cancel())
+        // 只看女生
+        let girlOnlyTitle = LocalizedString("LBS_Clear_MyLBS_Data_Button_Girl")
+        actionSheet.add(WXActionSheetItem(title: girlOnlyTitle, handler: { _ in
+            
+        }))
+        // 只看男生
+        let boyOnlyTitle = LocalizedString("LBS_Clear_MyLBS_Data_Button_Boy")
+        actionSheet.add(WXActionSheetItem(title: boyOnlyTitle, handler: { _ in
+            
+        }))
+        // 查看全部
+        let allTitle = LocalizedString("LBS_Clear_MyLBS_Data_Button_All")
+        actionSheet.add(WXActionSheetItem(title: allTitle, handler: { _ in
+            
+        }))
+        // 附近打招呼的人
+        let sayHelloTitle = LocalizedString("LBS_SayHello_MyLBS_Data_Button_All")
+        actionSheet.add(WXActionSheetItem(title: sayHelloTitle, handler: { _ in
+            
+        }))
+        // 清除位置信息并退出
+        let clearTitle = LocalizedString("LBS_Clear_MyLBS_Data_Button_Text")
+        actionSheet.add(WXActionSheetItem(title: clearTitle, handler: { _ in
+            
+        }))
+        actionSheet.show()
+    }
+    
+}
+
+// MARK: - ASTableDelegate & ASTableDataSource
+extension NearbyPeopleViewController: ASTableDelegate, ASTableDataSource {
+    
+    func numberOfSections(in tableNode: ASTableNode) -> Int {
+        return 1
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        let people = dataSource[indexPath.row]
+        let block: ASCellNodeBlock = {
+            return NearbyPeopleCellNode(people: people)
+        }
+        return block
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, didSelectRowAt indexPath: IndexPath) {
+        tableNode.deselectRow(at: indexPath, animated: false)
+        
+        let people = dataSource[indexPath.row]
+        let contactInfoVC = ContactInfoViewController(contact: people.toContact())
+        navigationController?.pushViewController(contactInfoVC, animated: true)
+    }
+}
