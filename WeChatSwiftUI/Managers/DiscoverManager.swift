272b000e70bb69199fdc59c669f071a97f4c864e
Jaward Sesay <jkef96@icloud.com>
2022-09-05 14:37:11 +0800

    managers

---
+++ WeChatSwiftUI/Managers/DiscoverManager.swift
@@ -0,0 +1,47 @@
+//
+//  DiscoverManager.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/06/20.
+//
+
+import Foundation
+
+class DiscoverManager {
+    
+    static let shared = DiscoverManager()
+    
+    private init() {
+        
+    }
+    
+    func discoverEntrance() -> [DiscoverSection] {
+        var sections: [DiscoverSection] = []
+        var moment = DiscoverModel(type: .moment, title: LocalizedString("FF_Entry_Album"), icon: "icons_outlined_colorful_moment")
+        moment.unreadCount = 2
+        sections.append(DiscoverSection(models: [moment]))
+        
+        sections.append(DiscoverSection(models: [
+            DiscoverModel(type: .scan, title: LocalizedString("FF_Entry_Scan"), icon: "icons_outlined_scan", color: Colors.Indigo),
+            DiscoverModel(type: .shake, title: LocalizedString("FF_Entry_Shake"), icon: "icons_outlined_shake", color: Colors.Indigo)]
+        ))
+        
+        sections.append(DiscoverSection(models: [
+            DiscoverModel(type: .news, title: LocalizedString("FF_Entry_Browse"), icon: "icons_outlined_news", color: Colors.Yellow),
+            DiscoverModel(type: .news, title: LocalizedString("FF_Entry_Search"), icon: "icons_filled_search-logo", color: Colors.Red)
+            ]))
+        
+        sections.append(DiscoverSection(models: [DiscoverModel(type: .nearby, title: LocalizedString("FF_Entry_PeopleNearBy"), icon: "icons_outlined_nearby", color: Colors.Indigo)]))
+        
+        if LanguageManager.shared.current == .simplefiedChinese {
+        sections.append(DiscoverSection(models: [
+            DiscoverModel(type: .shop, title: "购物", icon: "icons_outlined_shop", color: Colors.Orange),
+            DiscoverModel(type: .game, title: LocalizedString("MainFrame_RBtnGame"), icon: "icons_outlined_colorful_game")]))
+        } else {
+            sections.append(DiscoverSection(models: [ DiscoverModel(type: .game, title: LocalizedString("MainFrame_RBtnGame"), icon: "icons_outlined_colorful_game")]))
+        }
+        
+        sections.append(DiscoverSection(models: [DiscoverModel(type: .miniProgram, title: LocalizedString("WCAppBrand_Name"), icon: "icons_outlined_miniprogram", color: Colors.Purple)]))
+        return sections
+    }
+}
---
+++ WeChatSwiftUI/Managers/EmoticonManager.swift
@@ -0,0 +1,63 @@
+//
+//  EmoticonManager.swift
+//  WeChatSwift
+//
+//  Created by Jaykef on 2022/06/20.
+//
+
+import Foundation
+import SSZipArchive
+
+class EmoticonManager {
+ 
+    public private(set) var emoticons: [EmoticonViewModel] = []
+    
+    public private(set) var allStickers: [EmoticonPackage] = []
+    
+    public private(set) var allStickerPackageDesc: [StickerDescPackage] = []
+    
+    init() {
+        
+    }
+    
+    func setup() {
+        let expressionTabImage = UIImage.SVGImage(named: "icons_outlined_sticker")
+        emoticons.append(EmoticonViewModel(type: .expression, tabImage: expressionTabImage, emoticons: Expression.all))
+        
+        let favImage = UIImage.SVGImage(named: "icons_outlined_like")
+        
+        var favorites: [FavoriteEmoticon] = []
+        favorites.append(FavoriteEmoticon(type: .add))
+        favorites.append(FavoriteEmoticon(type: .jsb))
+        favorites.append(FavoriteEmoticon(type: .dice))
+        emoticons.append(EmoticonViewModel(type: .favorites, tabImage: favImage, emoticons: favorites))
+        
+        let takeImage = UIImage.SVGImage(named: "icons_outlined_takephoto_nor")
+        emoticons.append(EmoticonViewModel(type: .cameraEmoticon, tabImage: takeImage, emoticons: []))
+        
+        let path = NSHomeDirectory().appending("/Documents/emoticons/")
+        //try? FileManager.default.removeItem(atPath: path)
+        if !FileManager.default.fileExists(atPath: path) {
+            try? FileManager.default.createDirectory(at: URL(fileURLWithPath: path), withIntermediateDirectories: true, attributes: nil)
+            if let zipPath = Bundle.main.path(forResource: "Emoticons", ofType: "zip") {
+                SSZipArchive.unzipFile(atPath: zipPath, toDestination: path)
+            }
+        }
+        
+        if let jsonPath = Bundle.main.path(forResource: "Emoticons", ofType: "json"),
+            let jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonPath)) {
+            do {
+                let list = try JSONDecoder().decode([EmoticonPackage].self, from: jsonData)
+                allStickers = list
+                list.forEach { package in
+                    if let desc = package.toDescPackage() {
+                        allStickerPackageDesc.append(desc)
+                    }
+                }
+                list.forEach { emoticons.append($0.toEmoticonViewModel()) }
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
