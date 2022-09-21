dbb6c9cabb6bc7177e796a46f759f915d091d922
Jaward Sesay <jkef96@icloud.com>
2022-09-21 22:27:27 +0800

    Swift Charts

---
+++ SwiftChartsExampleApp.swift
@@ -0,0 +1,17 @@
+/*
+Created by Jaykef
+ 
+Abstract:
+The entry point into the app.
+*/
+
+import SwiftUI
+
+@main
+struct SwiftChartsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
