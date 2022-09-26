57851a0cc5d0c0d7ef0c7b6dd7c955348fcb9682
Jaward Sesay <jkef96@icloud.com>
2022-09-26 14:58:16 +0800

    app

---
+++ Explore/MapKitLookAround/AppDelegate.swift
@@ -0,0 +1,13 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The application delegate.
+*/
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
